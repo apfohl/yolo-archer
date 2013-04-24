931da0aea9c4039015ca267a060f2bdd0a62a2f1
bastinat0r <bastinat0r@bastinat0r.de>
2013-04-24 10:02:13 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoloArcher::Application.config.secret_token = '335c817c8d938b4bef3e45a8f1c20dbad25732f56a50d011550a4a50737fdfc30820883cfa388f7eaca9c7626fc20323abc95c8be06dfcb7bd5234533dcb6330'
