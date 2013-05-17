c61c9dff2d3f14d7b38f6aa1122bb34feb1caf6b
Andreas Pfohl <andreas@pf0hl.de>
2013-05-17 17:29:19 +0200

    closes #18 - markdown for annotations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,23 @@
 module ApplicationHelper
   def tag_cloud(tags, classes)
     max = 0
-
     tags.each do |t|
       if t.count.to_i > max
         max = t.count.to_i
       end
     end
-
     tags.each do |tag|
       index = tag.count.to_f / max * (classes.size - 1)
       yield(tag, classes[index.round])
     end
+  end
+
+  def markdown(text)
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true
+    }
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(text).html_safe
   end
 end
